feat: move mode calculation to Testra forward method and update model_worker to use correct output

main.py
@@ -1,6 +1,3 @@
-import argparse
-import threading
-
 import os
 import io
 import sys
@@ -8,8 +5,9 @@ import time
 import time
 import torch
 import requests
+import argparse
+import threading
 from PIL import Image
-from statistics import mode
 from torchvision import transforms
 
 import warnings
@@ -41,17 +39,12 @@ def model_worker(model, queue, frame_rate, server):
             frames = torch.stack(frames, dim=0)
             with torch.no_grad(), torch.autocast(device_type="cuda"):
                 output = model(frames)
-            output = torch.softmax(output, dim=1)
-            results = output.cpu().numpy()[0]
-            max_indices = results.argmax(axis=1)
-            mode_val = mode(max_indices) #TODO: move the mode calculation to the Testra forward method
-            print(f"Mode: {mode_val}")
 
             if server.last_client_ip:
                 callback_url = f"http://{server.last_client_ip}:{server.visor_callback_port}/callback/"
                 payload = {
                     "timestamp": time.time(),
-                    "action": int(mode_val)
+                    "action": int(output) #! TO CHANGE TO THE CORRECT OUTPUT
                 }
                 try:
                     response = requests.post(callback_url, json=payload)

src/testra/Testra.py
@@ -1,5 +1,7 @@
 import torch
+import numpy as np
 import torch.nn as nn
+from statistics import mode
 
 from src.testra.src.rekognition_online_action_detection.models import build_model
 from src.testra.src.rekognition_online_action_detection.utils.env import setup_environment
@@ -14,6 +16,10 @@ class Testra(nn.Module):
         self.cfg = cfg
         self.device = setup_environment(cfg)
         checkpointer = setup_checkpointer(cfg, phase='test')
+
+        # Percentage of prediction different from the background (0) 
+        # to calculate the mode of the predictions: 30%
+        self.threshold = float(cfg.MODEL.LSTR.WORK_MEMORY_LENGTH) * 30.0 / 100
 
         # Build backbone
         effnet = torch.hub.load('hankyul2/EfficientNetV2-pytorch', 'efficientnet_v2_s', pretrained=True)
@@ -31,11 +37,23 @@ class Testra(nn.Module):
         checkpointer.load(self.testra)
 
 
-    #TODO: include the mode calculation in here instead of outside
     def forward(self, x):
         with torch.no_grad(), torch.autocast(device_type="cuda"):
             x = self.backbone(x.to(self.device))
             x = x.unsqueeze(0)
             x = x.to(self.device)
             x = self.testra(x, x, x)
-        return x
+
+        # Mode calculation
+        output = torch.softmax(x, dim=1)
+        results = output.cpu().numpy()[0]
+        max_indices = results.argmax(axis=1)
+
+        idx_nonzero = np.where(max_indices != 0)[0]
+        print("Nonzero: ", idx_nonzero.shape[0])
+        print("Threshold: ", self.threshold)
+        if idx_nonzero.shape[0] <= self.threshold:
+            return 0
+
+        print("Mode: ", mode(max_indices[idx_nonzero]))
+        return mode(max_indices[idx_nonzero])