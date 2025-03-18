574ac36beb016367271c9fdcc1711c98dc6ac7c8
JustCati <ale.catinello@gmail.com>
2025-03-18 17:40:33 +0100

    chore: change folders positions

---
+++ src/testra/Testra.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+from src.testra.src.rekognition_online_action_detection.models import build_model
+from src.testra.src.rekognition_online_action_detection.utils.env import setup_environment
+from src.testra.src.rekognition_online_action_detection.utils.checkpointer import setup_checkpointer
+
+
+
+class Testra(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.cfg = cfg
+        self.device = setup_environment(cfg)
+        checkpointer = setup_checkpointer(cfg, phase='test')
+
+        # Build backbone
+        effnet = torch.hub.load('hankyul2/EfficientNetV2-pytorch', 'efficientnet_v2_s', pretrained=True)
+        self.backbone = nn.Sequential(*list(effnet.children())[:-1], 
+                                *list(list(effnet.children())[-1].children())[:-2],
+                                nn.AvgPool1d(kernel_size=257, stride=1))
+        self.backbone.to(self.device)
+        self.backbone.eval()
+
+        # Build testra
+        self.testra = build_model(cfg, self.device)
+        self.testra.to(self.device)
+        self.testra.eval()
+
+        checkpointer.load(self.testra)
+
+
+    def forward(self, x):
+        with torch.no_grad(), torch.autocast(device_type="cuda"):
+            x = self.backbone(x.to(self.device))
+            x = x.unsqueeze(0)
+            x = x.to(self.device)
+            x = self.testra(x, x, x)
+        return x
+
