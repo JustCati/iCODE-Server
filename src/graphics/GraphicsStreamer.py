fadd9a5e507620554b6da9462398c79686f24ded
JustCati <ale.catinello@gmail.com>
2025-03-22 20:59:15 +0100

    fix: simplify graphic_worker to process single frame and improve error handling

--- src/graphics/GraphicsStreamer.py
+++ src/graphics/GraphicsStreamer.py
@@ -10,27 +10,26 @@ class Streamer():
     def __init__(self, graphic_queue):
         self.graphic_queue = graphic_queue
 
-        self.graphic_worker_thread = threading.Thread(target=self.graphic_worker, daemon=True)
-        self.graphic_worker_thread.start()
+        # self.graphic_worker_thread = threading.Thread(target=self.graphic_worker, daemon=True)
+        # self.graphic_worker_thread.start()
 
 
     def graphic_worker(self):
         cv2.namedWindow("FrameStream", cv2.WINDOW_NORMAL)
 
         while True:
-            batch_frames = self.graphic_queue.get()
-            for frame_data in batch_frames:
-                try:
-                    img_array = np.frombuffer(frame_data, dtype=np.uint8)
-                    image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    if image is None:
-                        pil_img = Image.open(io.BytesIO(frame_data)).convert("RGB")
-                        image = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+            frame_data = self.graphic_queue.get()
+            try:
+                img_array = np.frombuffer(frame_data, dtype=np.uint8)
+                image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                if image is None:
+                    pil_img = Image.open(io.BytesIO(frame_data)).convert("RGB")
+                    image = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
 
-                    cv2.imshow("FrameStream", image)
-                    if cv2.waitKey(1) & 0xFF == 27: # ESC key
-                        break
-                except Exception as e:
-                    print("Error displaying frame:", e)
+                cv2.imshow("FrameStream", image)
+                if cv2.waitKey(1) & 0xFF == 27: # ESC key
+                    break
+            except Exception as e:
+                print("Error displaying frame:", e)
 
-            self.graphic_queue.task_done()
+        self.graphic_queue.task_done()
